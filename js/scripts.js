da391b76bd07307c187ca6027b458f257924fe55
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2017-01-23 23:56:41 +0100

    added google analytics for any link click
    currently this will only work for internal page clicks, because external link clicks are immediately actioned, which will not let ga to track

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-
-$(document).ready(function(){$('#sidebar').affix({
+$(document).ready(function(){
+    $('#sidebar').affix({
       offset: {
         top: 240
       }
-});	
+    });
+
+    $('a').click(function(){
+        var href = $(this).attr('href');
+        ga('send', 'event', 'linkclick', 'href', href);
+    });
 });
