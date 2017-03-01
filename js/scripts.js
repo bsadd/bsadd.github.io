451f22cbc1d7949791cac7657598db88c1751929
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2017-03-01 20:47:10 +0100

    Added sidebar for blog and prepared all blog loading into index

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,15 @@ function scrollToElement( $element , time = 500, success ){
         scrollTop: $element.offset().top
     }, time , success ); 
 }
+
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }else{
+       return results[1] || 0;
+    }
+};
+
+
