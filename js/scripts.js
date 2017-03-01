b5a1929847fd32becf449b2426d424e67228fa5a
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2017-03-01 23:02:46 +0100

    Added facebook comment on allposts and individual post pages

--- js/scripts.js
+++ js/scripts.js
@@ -48,4 +48,24 @@ $.urlParam = function(name){
     }
 };
 
+function loadFacebookCommentBox(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '937921679643234',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "http://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+}
+
+
 
